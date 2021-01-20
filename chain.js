0e375a94feac66f5f788acb901018f6b4f9483c2
SreenityaPonipireddy <75820726+SreenityaPonipireddy@users.noreply.github.com>
2021-01-19 21:08:50 -0600

    Add files via upload
    Will this work??

---
+++ chain.js
@@ -0,0 +1,20 @@
+class Chain{
+    constructor(mybodyA,mybodyB){
+      var option={
+        bodyA:mybodyA,
+        bodyB:mybodyB,
+        stiffness:1,
+        lenght:10
+      }  
+      this.chain=Constraint.create(option);
+      World.add(world,this.chain);
+
+    }
+   display(){
+       var pointA=this.chain.bodyA.position;
+       var pointB=this.chain.bodyB.position;
+       strokeWeight(3);
+       line(pointA.x,pointA.y,pointB.x,pointB.y);
+
+   }
+}
